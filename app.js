df53f8785866751136b56d089f72abd40942fbfe
Ansh Chauhan <chauhanansh44@gmail.com>
2025-09-05 10:53:43 +0530

    added new feature of adding new listing

--- app.js
+++ app.js
@@ -20,16 +20,39 @@ async function main() {
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true})); 
 
 
 app.get("/",(req,res)=>{
     res.send("hi");
 })
 
+// index route
 app.get("/listing",async(req,res)=>{
     const alllisting = await Listing.find({});
     res.render("listing/index.ejs",{alllisting});
 });
+
+app.get("/listing/new",(req,res)=>{
+    res.render("listing/new.ejs");
+})
+
+
+// show routes
+
+app.get("/listing/:id",async(req,res)=>{
+      let {id} = req.params;
+      let listing = await Listing.findById(id);
+      res.render("listing/show.ejs",{listing});
+})
+
+
+// adding new listing
+app.post("/listing",async(req,res)=>{
+   let newlisting = new Listing(req.body.listing);
+  await newlisting.save();
+  res.redirect("/listing");
+})
 
 
 // app.get("/listingtesting",async(req,res)=>{
