96fffcc1d383746decc1ce9d521421f2df1523b1
Ansh Chauhan <chauhanansh44@gmail.com>
2025-09-05 17:06:53 +0530

    updated server

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const mongoose = require('mongoose');
 const Listing = require("../AaramStay/models/listing.js");
 var methodOverride = require('method-override')
+const ejsMate = require('ejs-mate');
 const MONGO_URL = "mongodb://127.0.0.1:27017/AaramStay";
 
 main()
@@ -22,6 +23,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true})); 
 app.use(methodOverride("_method"))
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")))
 
 app.get("/",(req,res)=>{
     res.send("hi");
