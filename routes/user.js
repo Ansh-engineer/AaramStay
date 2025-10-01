060936c174ce29ecce8e75c73eaab2e5efb0e83e
Ansh Chauhan <chauhanansh44@gmail.com>
2025-10-01 11:53:55 +0530

    review styling   updated

--- routes/user.js
+++ routes/user.js
@@ -6,14 +6,23 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware");
 const userscontrollers = require("../controllers/users");
 
-router.get("/signup",userscontrollers.rendersignup);
+router
+  .route("/signup")
+  .get( userscontrollers.rendersignup)
+  .post( wrapAsync(userscontrollers.signup));
 
-router.post("/signup", wrapAsync (userscontrollers.signup));
+router
+  .route("/login")
+  .get(userscontrollers.renderlogin)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userscontrollers.login
+  );
 
-router.get("/login",userscontrollers.renderlogin);
-
-router.post("/login",saveRedirectUrl, passport.authenticate("local",{failureRedirect:"/login" , failureFlash:true})  ,userscontrollers.login);
-
-router.get("/logout",userscontrollers.logout);
+router.get("/logout", userscontrollers.logout);
 
 module.exports = router;
